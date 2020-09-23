07198a7f217590444c4d6d5a6f72c4cb187d1e26
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-22 22:05:13 -0400

    beginning stages

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,5 @@
+//node modules needed to make and delete notes in the app
+var mysql = require ("mysql");
+var express = require ("express");
+var apiRoutes = require ("./routes/apiRoutes");
+var htmlRoutes = require ("./routes/htmlRoutes")
