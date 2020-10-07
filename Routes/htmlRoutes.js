cefedcd1266b67ff2c82eb68bb88d047ff3367aa
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-06 22:43:11 -0400

    finished htmlroutes with ref notes

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,7 +1,12 @@
 const path = require("path");
 
+//Get and send information to the index.html
 module.exports = app => {
-    app.get("\", (req,res)==>
-    resizeBy.sendFile(path.join(__dirname, "..public/index.html"))
+    app.get("/", (req,res) =>
+    res.sendFile(path.join(__dirname, "..public/index.html"))
     );
-}
+//Get and send information to the notes.html
+    app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+    );
+};
