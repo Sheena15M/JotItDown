42e9630a58cb2d59785d9a82d01494c8f10543aa
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-07 12:33:01 -0400

    some bug fixes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,5 +1,4 @@
 //node modules needed to make and delete notes in the app
-var mysql = require ("mysql");
 var express = require ("express");
 var apiRoutes = require ("./routes/apiRoutes");
 var htmlRoutes = require ("./routes/htmlRoutes")
