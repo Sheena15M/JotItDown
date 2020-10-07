910044b4ad8c7dfd63649f717259d745ca0a449a
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-07 13:36:13 -0400

    complete server.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,3 +2,20 @@
 var express = require ("express");
 var apiRoutes = require ("./routes/apiRoutes");
 var htmlRoutes = require ("./routes/htmlRoutes")
+
+//When you use express, you need a local host port, live server won't run it
+var PORT = process.env.PORT || 8080;
+
+//Allows Express to handle the data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Air traffic control, require the routes for the server
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Make sure the PORT is listening, or it won't work
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+}
+);
