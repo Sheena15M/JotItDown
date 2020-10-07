564fc9dba3e9f18ab7c0e219e4808737d8283587
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-06 22:13:22 -0400

    notes added throughout as reminders

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,6 +1,8 @@
+//NPM packages needed for the app
 const fs = require ("fs");
 const path = require ("path");
 
+//JSON to create the pathways
 const notesData = JSON.parse(
     fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, data) => {
         if (err) throw err;
@@ -8,6 +10,7 @@ const notesData = JSON.parse(
     )
 );
 
+//Function needed to write the notes in the first place
 function writeNotesData(notesData) {
     fs.writeFileSync(
         path.join(__dirname, "../db/db.json"),
@@ -18,12 +21,14 @@ function writeNotesData(notesData) {
     );
 }
 
+//Function to get the notes data regardless of how long it is
 function getID(notesData) {
     if (notesData.length === 0) {
         return 0;
     }
 }
 
+//Sort the notes in order
 notesData.sort((a, b) => a.id - b.id);
 
 const nextIndexAfterLast = notesData.length;
@@ -31,4 +36,10 @@ for (let i = 0; i < nextIndexAfterLast; i++) {
     if (notesData[i].id !== i) {
         return i;
     }
+}
+return nextIndexAfterLast;
+
+//Get the notes, post them, and if you're done delete them 
+module.exports = app => {
+    app.get("/api/notes", (req, res) => res.json(notesData));
 }
